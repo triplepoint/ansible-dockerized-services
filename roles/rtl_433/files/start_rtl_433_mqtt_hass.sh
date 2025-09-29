7e292688072e0bf112ff9f3e637cdc880ddded34
Jonathan Hanson <jonathan@jonathan-hanson.org>
2025-09-29 13:27:50 -0700

    better formatting of the rtl433 discovery script

--- roles/rtl_433/files/start_rtl_433_mqtt_hass.sh
+++ roles/rtl_433/files/start_rtl_433_mqtt_hass.sh
@@ -1,3 +1,8 @@
-python3 -u ./rtl_433_mqtt_hass.py --host ${MQTT_HOST} --port ${MQTT_PORT} --ca_cert ${MQTT_CA_CERT_PATH} \
-  --rtl-topic ${RTL_TOPIC} --discovery-prefix ${DISCOVERY_PREFIX} --device-topic_suffix ${DEVICE_TOPIC_SUFFIX} \
+python3 -u ./rtl_433_mqtt_hass.py \
+  --host ${MQTT_HOST} \
+  --port ${MQTT_PORT} \
+  --ca_cert ${MQTT_CA_CERT_PATH} \
+  --rtl-topic ${RTL_TOPIC} \
+  --discovery-prefix ${DISCOVERY_PREFIX} \
+  --device-topic_suffix ${DEVICE_TOPIC_SUFFIX} \
   --interval ${DISCOVERY_INTERVAL} ${OTHER_ARGS}
